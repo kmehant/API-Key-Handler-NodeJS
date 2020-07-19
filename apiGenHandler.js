2f84bc571d5998ab56a06173ca7c9070cc28d3ea
K mehant <411843@student.nitandhra.ac.in>
2020-07-19 15:55:52 +0530

    Initial Commit q
    Signed-off-by: K mehant <411843@student.nitandhra.ac.in>

---
+++ apiGenHandler.js
@@ -0,0 +1,153 @@
+var SortedSet = require("collections/sorted-set");
+var crypto = require("crypto");
+
+class apiGenHandler {
+    constructor() {
+        this.keys = {}
+        this.activeCount = 0
+        this.blockedQueue = new SortedSet([])
+        this.activeSet = new SortedSet()
+        this.blockedQueue.contentCompare = function (a, b) {
+            return a["ts"] < b["ts"]
+        }
+        this.blockedQueueHandler = false
+    }
+
+    genApiKey() {
+        var ret = false
+        var apiKey = crypto.randomBytes(32).toString('hex');
+        try {
+            var d = new Date()
+            this.keys[apiKey] = {}
+            this.keys[apiKey]["state"] = 1
+            this.keys[apiKey]["ts"] = d
+            console.log(this.keys)
+            var c = this.activeCount
+            c = c + 1
+            this.activeCount = c
+            this.activeSet.add(apiKey)
+            ret = true
+        } catch (err) {
+            console.log(err.message)
+            ret = false
+        }
+        return ret
+    }
+
+    getAvailableAPIKey() {
+        var c = this.activeCount
+        if (c == 0) {
+            return false
+        }
+        c = c - 1
+        this.activeCount = c
+        var ret = false
+        try {
+            var ranVal = Math.random() * (c - 0) + 0
+            var tempArray = this.activeSet.toArray()
+            var apiKey = tempArray[ranVal]
+            console.log(apiKey)
+            this.activeSet.remove(apiKey)
+            var d = new Date()
+            d.setMinutes(d.getMinutes() + 5)
+
+            this.keys[apiKey]["state"] = 0
+            this.keys[apiKey]["ts"] = d
+            var element = this.keys[apiKey]
+            element["api"] = apiKey
+            this.blockedQueue.add(element)
+            ret = apiKey
+        } catch (err) {
+            console.log(err.message)
+            ret = false
+        }
+        // Write a worker which will delete the blocked API keys
+        if (this.blockedQueueHandler == false) {
+            this.blockedQueueHandler = true
+            var p = this.killAPI()
+        }
+
+        return ret
+    }
+
+    unblockAPIKey(key) {
+        var ret = false
+        try {
+            if (this.keys[key]["state"] == 1) {
+                return ret
+            }
+            this.keys[key]["state"] = 1
+            var c = this.activeCount
+            c = c + 1
+            this.activeCount = c
+            this.activeSet.add(key)
+            ret = true
+        } catch (err) {
+            ret = false
+            console.log(err.message)
+        }
+        return ret
+    }
+
+    deleteAPIKey(key) {
+        var ret = false
+        try {
+            if (this.keys[key]["state"] == 1) {
+                this.activeSet.remove(key)
+                var c = this.activeCount
+                c = c - 1
+                self.activeCount = c
+            }
+            delete this.keys[key]
+            ret = true
+        } catch (err) {
+            console.log(err.message)
+            ret = false
+        }
+        return ret
+    }
+
+    pollApiKey(key) {
+        var ret = false
+        try {
+            var timeStamp = this.keys[key]["ts"]
+            this.blockedQueue.delete(this.keys[key])
+            timeStamp.setMinutes(timeStamp.getMinutes() + 5)
+            this.keys[key]["ts"] = timeStamp
+            element = this.keys[key]
+            element["api"] = key
+            this.blockedQueue.add(element)
+            ret = true
+        } catch (err) {
+            console.log(err.message)
+            ret = false
+        }
+        return ret
+    }
+
+    killAPI() {
+        var popIt = true
+        var element
+        while (true) {
+        return new Promise((resolve, reject) => {
+                if (popIt == true) {
+                    element = this.blockedQueue.pop()
+                    popIt = false
+                }
+                var presentTime = new Date()
+                try {
+                    if (presentTime > element["ts"]) {
+                        delete this.keys[element["api"]]
+                        popIt = true
+                    }
+                } catch (err) {
+                    console.log(err.message)
+                }
+            
+        })
+    }
+
+    }
+}
+
+module.exports.apiGenHandler = apiGenHandler
---
+++ index.js
@@ -0,0 +1,81 @@
+// imports
+const express = require('express')
+
+
+// constants
+const SERVER_PORT = 3000
+
+const apiGenHandler = require('./apiGenHandler')
+
+const handler = new apiGenHandler.apiGenHandler()
+
+const app = express()
+
+app.get('/', function (req, res) {
+
+    return res.status(200).json({ "message": "API Key Generator Assignment from Codeuino for LF Mentorship" }).send()
+})
+
+app.post('/key', function (req, res) {
+    var stat = handler.genApiKey()
+    var code = 200
+    var resp = { "message": "Generated" }
+    if (stat == false) {
+        resp.message = "Failed"
+        code = 404
+    }
+    return res.status(code).json(resp).send()
+})
+
+app.get('/key', function (req, res) {
+    var key = handler.getAvailableAPIKey()
+    var resp = { "message": "Success", "key": key }
+    var code = 200
+    if (key == false) {
+        resp.message = "Failed"
+        code = 404
+    }
+    return res.status(code).json(resp).send()
+})
+
+// Route to unblock the given key
+app.post('/key/unblock', function (req, res) {
+    var key = req.query.api
+    var stat = handler.unblockAPIKey(key)
+    var resp = {"message": "Success"}
+    var code = 200
+    if (stat == false) {
+        resp.message = "Failed"
+        code = 404
+    }
+    return res.status(code).json(resp).send()
+})
+
+// Route to delete the given key
+app.delete('/key', function (req, res) {
+    var key = req.query.api
+    var stat = handler.deleteAPIKey(key)
+    var resp = {"message": "Success"}
+    var code = 200
+    if (stat == false) {
+        resp.message = "Failed"
+        code = 404
+    }
+    return res.status(code).json(resp).send()
+})
+
+// Route for users to keep their keys active
+app.post('/key/poll', function (req, res) {
+    var key = req.query.api
+    var stat = handler.pollApiKey(key)
+    var resp = {"message": "Success"}
+    var code = 200
+    if (stat == false) {
+        resp.message = "Failed"
+        code  = 404
+    }
+    return res.status(404).json(resp).send()
+})
+
+
+app.listen(SERVER_PORT, () => console.log(`API Key Gen is listening at http://localhost:${SERVER_PORT}`))
